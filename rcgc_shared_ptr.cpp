f24bdc241f04466c8d31e566a90b59cac0f46734
john2015 <yyl_20050115@hotmail.com>
2020-07-30 05:43:30 +0800

    tabs->spaces

--- rcgc_shared_ptr.cpp
+++ rcgc_shared_ptr.cpp
@@ -8,89 +8,89 @@ std::vector<void*> rcgc_base::_wilds;
 
 size_t rcgc_base::NextIndex()
 {
-	return index++;
+    return index++;
 }
 
 //this relationship is simplified,
 //and represented by counting plus one.
 void rcgc_base::AddRelation(void* ptr, size_t id)
 {
-	if (ptr != nullptr) {
-		std::lock_guard<std::mutex> lock(m);
-		auto p = _refs.find(ptr);
-		if (p == _refs.end()) {
-			_refs.insert(std::make_pair(ptr, 1));
-		}
-		else {
-			p->second++;
-		}
-	}
+    if (ptr != nullptr) {
+        std::lock_guard<std::mutex> lock(m);
+        auto p = _refs.find(ptr);
+        if (p == _refs.end()) {
+            _refs.insert(std::make_pair(ptr, 1));
+        }
+        else {
+            p->second++;
+        }
+    }
 }
 
 void rcgc_base::RemoveRelation(void* ptr, size_t id)
 {
-	if (ptr != nullptr) {
-		std::lock_guard<std::mutex> lock(m);
-		auto p = _refs.find(ptr);
-		if (p != _refs.end()) {
-			if (--p->second == 0) {
-				_refs.erase(p);
-				_wilds.push_back(ptr);
-			}
-		}
-	}
+    if (ptr != nullptr) {
+        std::lock_guard<std::mutex> lock(m);
+        auto p = _refs.find(ptr);
+        if (p != _refs.end()) {
+            if (--p->second == 0) {
+                _refs.erase(p);
+                _wilds.push_back(ptr);
+            }
+        }
+    }
 }
 
 bool rcgc_base::SetAutoCollect(bool _ac)
 {
-	return ac=_ac;
+    return ac = _ac;
 }
 
 bool rcgc_base::GetAutoCollect()
 {
-	return ac;
+    return ac;
 }
 
 void rcgc_base::Collect(bool threading, bool join)
 {
-	if (_wilds.size() > 0) {
-		if (threading) {
-			std::thread t(CollectThread);
-			if (join) {
-				t.join();
-			}
-			else {
-				t.detach();
-			}
-		}
-		else {
-			Collect(_wilds);
-		}
-	}
+    if (_wilds.size() > 0) {
+        if (threading) {
+            std::thread t(CollectThread);
+            if (join) {
+                t.join();
+            }
+            else {
+                t.detach();
+            }
+        }
+        else {
+            Collect(_wilds);
+        }
+    }
 }
 
 void rcgc_base::CollectThread()
 {
-	std::vector<void*> p_wilds;
-	{
-		std::lock_guard<std::mutex> lock(m);
-		p_wilds = _wilds;
-		_wilds.clear();
-	}
-	Collect(p_wilds);
+    std::vector<void*> p_wilds;
+    {
+        std::lock_guard<std::mutex> lock(m);
+        p_wilds = _wilds;
+        _wilds.clear();
+    }
+    Collect(p_wilds);
 }
 
 void rcgc_base::Collect(std::vector<void*>& p_wilds)
 {
-	if (p_wilds.size() > 0) {
-		for (auto p = p_wilds.begin(); p != p_wilds.end(); ++p) {
-			if (*p) {
-				//delete p->first;
-				//NOTICE: use free instead of delete to avoid double calling destructor
-				free(*p);
-			}
-		}
-		p_wilds.clear();
-	}
+    if (p_wilds.size() > 0) {
+        for (auto p = p_wilds.begin(); p != p_wilds.end(); ++p) {
+            if (*p) {
+                //delete p->first;
+                //NOTICE: use free instead of delete to avoid double calling destructor
+                free(*p);
+            }
+        }
+        p_wilds.clear();
+    }
 }
 
--- rcgc_shared_ptr.h
+++ rcgc_shared_ptr.h
@@ -6,72 +6,72 @@
 
 class rcgc_base {
 public:
-	static bool SetAutoCollect(bool ac);
-	static bool GetAutoCollect();
-	static void Collect(bool threading =false, bool join = false);
+    static bool SetAutoCollect(bool ac);
+    static bool GetAutoCollect();
+    static void Collect(bool threading = false, bool join = false);
 protected:
-	static size_t NextIndex();
-	static void AddRelation(void* ptr, size_t id);
-	static void RemoveRelation(void* ptr, size_t id);
-	static void CollectThread();
-	static void Collect(std::vector<void*>& p_wilds);
+    static size_t NextIndex();
+    static void AddRelation(void* ptr, size_t id);
+    static void RemoveRelation(void* ptr, size_t id);
+    static void CollectThread();
+    static void Collect(std::vector<void*>& p_wilds);
 protected:
-	static bool ac;
-	static size_t index;
-	static std::mutex m;
-	static std::unordered_map<void*, size_t> _refs;
-	static std::vector<void*> _wilds;
+    static bool ac;
+    static size_t index;
+    static std::mutex m;
+    static std::unordered_map<void*, size_t> _refs;
+    static std::vector<void*> _wilds;
 };
 
 template<class PTR>
-class rcgc_shared_ptr 
-	: public rcgc_base
+class rcgc_shared_ptr
+    : public rcgc_base
 {
 public:
 
-	rcgc_shared_ptr(PTR* ptr = nullptr) :_ptr(ptr), _id(NextIndex()) {
-		AddRelation(this->_ptr, this->_id);
-	}
-	rcgc_shared_ptr(const rcgc_shared_ptr& src):_ptr(src._ptr), _id(NextIndex()) {
-		AddRelation(this->_ptr, this->_id);
-	}
-	virtual ~rcgc_shared_ptr() {
-		this->Dispose();
-		if (ac) {
-			Collect();
-		}
-	}
+    rcgc_shared_ptr(PTR* ptr = nullptr) :_ptr(ptr), _id(NextIndex()) {
+        AddRelation(this->_ptr, this->_id);
+    }
+    rcgc_shared_ptr(const rcgc_shared_ptr& src) :_ptr(src._ptr), _id(NextIndex()) {
+        AddRelation(this->_ptr, this->_id);
+    }
+    virtual ~rcgc_shared_ptr() {
+        this->Dispose();
+        if (ac) {
+            Collect();
+        }
+    }
 public:
-	void Dispose() {
-		if (this->_ptr != nullptr) {
-			PTR* _ptr = this->_ptr;
-			this->_ptr = nullptr;
+    void Dispose() {
+        if (this->_ptr != nullptr) {
+            PTR* _ptr = this->_ptr;
+            this->_ptr = nullptr;
 
-			_ptr->~PTR();
+            _ptr->~PTR();
 
-			RemoveRelation(_ptr, _id);
-		}
-	}
-	rcgc_shared_ptr& operator = (rcgc_shared_ptr<PTR>& src) {
-		if (this->_ptr == src._ptr)
-			return *this;
-		else if (src._ptr != nullptr) {
-			RemoveRelation(this->_ptr, this->_id);
-			AddRelation(this->_ptr = src._ptr, this->_id);
-		}
-		return *this;
-	}
+            RemoveRelation(_ptr, _id);
+        }
+    }
+    rcgc_shared_ptr& operator = (rcgc_shared_ptr<PTR>& src) {
+        if (this->_ptr == src._ptr)
+            return *this;
+        else if (src._ptr != nullptr) {
+            RemoveRelation(this->_ptr, this->_id);
+            AddRelation(this->_ptr = src._ptr, this->_id);
+        }
+        return *this;
+    }
 
-	PTR& operator*() {
-		return *_ptr;
-	}
-	PTR* operator->() {
-		return _ptr;
-	}
-	long long GetId() {
-		return this->_id;
-	}
+    PTR& operator*() {
+        return *_ptr;
+    }
+    PTR* operator->() {
+        return _ptr;
+    }
+    long long GetId() {
+        return this->_id;
+    }
 protected:
-	const size_t _id;
-	PTR* _ptr;
+    const size_t _id;
+    PTR* _ptr;
 };
