5c342e00b8023752d099305a861a477723eec1be
john2015 <yyl_20050115@hotmail.com>
2020-08-01 00:56:33 +0800

    added test case1: class A is renamed to class C

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <crtdbg.h>
 #endif
+
+#include <iostream>
 #include "rcgc_shared_ptr.h"
 
 class B;
@@ -26,6 +28,36 @@ public:
     rcgc_shared_ptr<A> _ptr_outA;
 };
 
+
+struct C
+{
+    unsigned int x = 0u;
+
+    ~C() { x = 0xBAADF00D; }
+};
+
+//struct E;
+//
+//struct D
+//{
+//    rcgc_shared_ptr< E > m_b;
+//    std::string m_x;
+//
+//    ~D() { std::cout << m_b->m_x << std::endl; }
+//};
+//
+//struct E
+//{
+//    rcgc_shared_ptr< D > m_a;
+//    std::string m_x;
+//
+//    ~E() { std::cout << m_a->m_x << std::endl; }
+//};
+//
+
+
+
+
 //Here is a simple method to solve circulating reference problem of shared_ptr without using weak_ptr.
 int main()
 {
@@ -41,4 +73,27 @@ int main()
     ptr_A->_ptr_outB1 = ptr_B;
     ptr_A->_ptr_outB2 = ptr_B;
     ptr_B->_ptr_outA = ptr_A;
+
+    /// <summary>
+    /// case 1
+    /// </summary>
+    /// <returns></returns>
+    rcgc_shared_ptr< C > p1;
+    {
+        rcgc_shared_ptr< C > p2(new C());
+        p1 = p2;
+        std::cout <<std::hex<< p1->x << std::endl;
+    }
+    std::cout << std::hex << p1->x << std::endl; // (1)
+
+    /// <summary>
+    /// case 2
+    /// </summary>
+    /// <returns></returns>
+    //rcgc_shared_ptr< D > p(new D());
+    //rcgc_shared_ptr< E > p2(new E());
+    //
+    //p->m_b->m_a = p;
+
+
 }
